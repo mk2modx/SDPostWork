d6eda42f1d7a98d8dcc1bcaf8812e70dece7b254
Mark Ag <mk2modx@icloud.com>
2019-10-31 10:21:07 -0600

    JTHW Ex 40

---
+++ JTHWComplete/ReceiptRevisited.java
@@ -0,0 +1,31 @@
+
+import java.io.PrintWriter;
+
+public class ReceiptRevisited {
+	public static void main(String[] args) throws Exception {
+		PrintWriter fileout = new PrintWriter("receipt.txt");
+
+		fileout.println("+------------------------------+");
+		fileout.println("|                              |");
+		fileout.println("|                              |");
+		fileout.println("|         CORNER STORE         |");
+		fileout.println("|                              |");
+		fileout.println("|   2019-10-31      04:38PM    |");
+		fileout.println("|                              |");
+		fileout.println("|                              |");
+		fileout.println("|   Gallons:        12.464     |");
+		fileout.println("|   Price/gallon:  $ 3.459     |");
+		fileout.println("|                              |");
+		fileout.println("|                              |");
+		fileout.println("|   Fuel total:    $ 43.11     |");
+		fileout.println("|                              |");
+		fileout.println("|                              |");
+		fileout.println("|                              |");
+		fileout.println("+------------------------------+");
+		fileout.close();
+
+}
+
+
+}
+
