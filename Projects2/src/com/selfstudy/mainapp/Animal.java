dfbf8bf0fb2bb5e6eae95cf684eba248305f6e09
Mark Ag <mk2modx@icloud.com>
2019-11-07 16:06:15 -0700

    Animal Stubbed out

--- Projects2/src/com/selfstudy/mainapp/Animal.java
+++ Projects2/src/com/selfstudy/mainapp/Animal.java
@@ -2,4 +2,37 @@ package com.selfstudy.mainapp;
 
 public class Animal {
 
+	private String firstName;
+
+	public Animal(String firstName) {
+		super();
+		this.firstName = firstName;
+	}
+
+	public Animal() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Animal [firstName=" + firstName + "]";
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+	public void eat(int food) {
+		System.out.println("I ate " + food + " lbs of food.");
+	}
+	
+	public void makeNoise() {
+		System.out.println("I am a " + this.getClass().getSimpleName());
+	}
+	
 }
